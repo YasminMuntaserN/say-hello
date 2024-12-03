eeb24891773397a2b919bfa344c60e357c94670e
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-03 18:59:17 +0200

    feat: add user entity

---
+++ sayHello.Backend/sayHello.Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace sayHello.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ProfilePictureUrl { get; set; }
+    public string Bio { get; set; }
+    public string Status { get; set; } //Online, Offline, Last Seen
+    public DateTime DateJoined { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? LastLogin { get; set; }
+}
