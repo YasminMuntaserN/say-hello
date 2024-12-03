d5a8bd1c44e1dc34f2ed635deffaca1fcd740fc8
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-03 18:50:06 +0200

    feat: add AppDbContext file

---
+++ sayHello.Backend/sayHello.DataAccess/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace sayHello.DataAccess;
+
+public class AppDbContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config.GetConnectionString("Connection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder); 
+    }
+}
