1e301d4b5bdbde38d527a4f4cd991baab0d9573c
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-05 14:37:48 +0200

    feat: add AutoMapper and FluentValidation packages

--- sayHello.Frontend/src/App.jsx
+++ sayHello.Frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import {BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Dashboard from "./pages/Dashboard";
+
+
+
 function App() {
-
   return (
-    <>
-       heelo
-    </>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
