bde82fd0a08c635b42aa37ae978b7c99c5b2549a
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-14 12:57:56 +0200

    feat: add form container component to make it reusable to use in different forms

--- sayHello.Frontend/src/ui/FormContainer.jsx
+++ sayHello.Frontend/src/ui/FormContainer.jsx
@@ -10,5 +10,5 @@ function FormContainer({ header , children}) {
 export default FormContainer
 
 
-const StyledContainer = "bg-white z-1000 rounded-3xl shadow-lg mx-auto mt-10 w-3/4 h-max  p-10 overflow-hidden";
+const StyledContainer = "bg-white z-1000 rounded-3xl shadow-lg mx-auto mt-10  sm:w-[150%] lg:w-3/4 md:w-[100%] lg:ml-auto p-10";
 const StyledH = "text-3xl font-bold mb-5 text-basic overflow-hidden";
