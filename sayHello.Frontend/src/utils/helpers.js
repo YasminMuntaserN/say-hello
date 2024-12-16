d95112cb2f99f173bbb4ec1617efbf482bea6d46
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-16 14:55:52 +0200

    feat: add helper class to add all methods that shared between classes

---
+++ sayHello.Frontend/src/utils/helpers.js
@@ -0,0 +1,16 @@
+import { format } from "date-fns";
+
+export function formatTime(isoDateString) {
+  console.log(isoDateString);
+  if (!isoDateString) return "Invalid Date";
+
+  let normalizedDate = isoDateString;
+  if (isoDateString.match(/T\d{2}:\d{2}:\d{2}\.\d{2}$/)) {
+    normalizedDate = isoDateString + "0";
+  }
+
+  const date = new Date(normalizedDate);
+  if (isNaN(date)) return "Invalid Date";
+
+  return format(date, "h:mma").toLowerCase();
+}
