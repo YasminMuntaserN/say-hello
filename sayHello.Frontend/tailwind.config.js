bbf74ce07ea735423ad348855e0765d84c098765
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-21 09:18:14 +0200

    install react dev tools

--- sayHello.Frontend/tailwind.config.js
+++ sayHello.Frontend/tailwind.config.js
@@ -3,6 +3,25 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      extend: {
+        animation: {
+          l3: "l3 1s infinite linear",
+        },
+        keyframes: {
+          l3: {
+            "20%": { backgroundPosition: "0% 0%, 50% 50%, 100% 50%" },
+            "40%": { backgroundPosition: "0% 100%, 50% 0%, 100% 50%" },
+            "60%": { backgroundPosition: "0% 50%, 50% 100%, 100% 0%" },
+            "80%": { backgroundPosition: "0% 50%, 50% 50%, 100% 100%" },
+          },
+        },
+        spacing: {
+          "loader-width": "60px",
+        },
+        aspectRatio: {
+          loader: "2",
+        },
+      },
       fontFamily: {
         sans: ["Titillium Web", "sans-serif"],
         regular: ["Titillium Web", "sans-serif"],
@@ -12,13 +31,15 @@ export default {
         basic: "#091d2f",
         secondary: "#513c5a",
         lightText: "#818388",
-        purple: "#560D49",
+        purple: "#632969",
         gray: "#e5e7eb",
         blue: "#083869",
       },
       backgroundImage: {
         "gradient-bg": "linear-gradient(to right, #280B31, #04213D)",
         "gradient-btn": "linear-gradient(to top, #632969,#9D38D2, #2E8DEC)",
+        "gradient-message":
+          "linear-gradient(to right, #632969,#9D38D2, #2E8DEC)",
       },
     },
   },
