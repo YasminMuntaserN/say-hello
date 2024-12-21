17201f3345a276a762d665cb47681a255703c67c
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-21 09:30:35 +0200

    feat: add hooks for check if the user exist by email and password and hook for get all messages send by the user

---
+++ sayHello.Frontend/src/hooks/Users/useAllUsersBySenderId.js
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { getAllBySenderId } from "../../services/apiUser";
+
+export function useAllUsersBySenderId() {
+  const {
+    mutate,
+    isLoading,
+    error,
+    data: AllUsers,
+  } = useMutation({
+    mutationKey: ["Messages"],
+    mutationFn: getAllBySenderId,
+    onError: (err) => {
+      console.log(`${err.message}`);
+    },
+  });
+
+  return { mutate, isLoading, error, AllUsers };
+}
---
+++ sayHello.Frontend/src/hooks/Users/useExistUser.js
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { handleCheckUserByEmailAndPassword } from "../../services/apiUser";
+import { toast } from "react-hot-toast";
+
+export function useExistUser() {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate,
+    isLoading,
+    error,
+    data: User,
+  } = useMutation({
+    mutationFn: ({ Email, Password }) =>
+      handleCheckUserByEmailAndPassword(Email, Password),
+    onSuccess: (User) => {
+      if (User) {
+        toast.success(`Welcome ${User.username}`);
+        queryClient.invalidateQueries({ queryKey: ["Users"] });
+      } else {
+        toast.error(`There is no user found for the given email and password.`);
+      }
+    },
+    onError: (err) => {
+      console.error("An error occurred:", err);
+      toast.error(`user not found 🥲`);
+    },
+  });
+
+  return { mutate, isLoading, error, User };
+}
