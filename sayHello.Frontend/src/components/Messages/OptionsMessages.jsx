3a39a04b93c32035b92dc8198ad0053e398a6276
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-25 11:29:41 +0200

    feat: add OptionsMessages component for quick message selection and sending

---
+++ sayHello.Frontend/src/components/Messages/OptionsMessages.jsx
@@ -0,0 +1,34 @@
+const messages = [
+  "Hello !",
+  "How Are You ?",
+  "As-salamu alaykum 👋",
+];
+
+function OptionsMessages({ addMessage }) {
+
+
+  const handleSend = (message) => {
+    if (message.trim()) {
+      addMessage(message);
+    }
+  };
+  return (
+    <div className={optionsContainer}>
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={StyledMessage}
+          onClick={() => handleSend(message)}
+        >
+          {message}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const StyledMessage =
+  "bg-gradient-message cursor-pointer rounded-tl-full text-lg px-5 rounded-full  m-3 p-3 w-fit text-white hover:bg-gradient-to-r hover:from-blue-500 hover:bg-secondary";
+const optionsContainer ="flex-grow flex justify-center items-end h-[520px]";  
+
+export default OptionsMessages;
