a8d992cc5631e21769ea550bc204c4a7183aee9d
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-31 09:35:01 +0200

    feat: add group context

---
+++ sayHello.Frontend/src/context/GroupContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState } from "react";
+import { useAddGroupMember } from "../components/Groups/hooks/useGroupMember";
+import { useChat } from "./UserContext";
+
+const GroupContext = createContext();
+
+export function GroupProvider({ children }) {
+  const {user}=useChat();
+  const [MemberInGroup, setMemberInGroup] = useState([user.userId]);
+  const { mutate } = useAddGroupMember();
+
+  const addGroupMember = (userId) => {
+    setMemberInGroup((prev) => 
+      prev.includes(userId) ? prev : [...prev, userId]
+    );
+  };
+
+  const RemoveGroupMember = (userId) => {
+    setMemberInGroup((prev) => prev.filter((memberId) => memberId !== userId));
+  };
+
+  const SaveGroupMembers = async (groupId) => {
+    try {
+      await Promise.all(
+        MemberInGroup.map((userId) =>
+          mutate({ groupId, userId }, {
+            onSuccess: (data) => {
+              console.log("groupMember", data);
+            },
+            onError: (error) => {
+              console.error("Error adding group member:", error);
+            },
+          })
+        )
+      );
+    } catch (error) {
+      console.error("Error saving group members:", error);
+    }
+  };
+
+  return (
+    <GroupContext.Provider
+      value={{
+        MemberInGroup,
+        setMemberInGroup,
+        addGroupMember,
+        RemoveGroupMember,
+        SaveGroupMembers,
+      }}
+    >
+      {children}
+    </GroupContext.Provider>
+  );
+}
+
+export function useGroup() {
+  const context = useContext(GroupContext);
+  if (!context) {
+    throw new Error("useGroup must be used within a GroupProvider");
+  }
+  return context;
+}
