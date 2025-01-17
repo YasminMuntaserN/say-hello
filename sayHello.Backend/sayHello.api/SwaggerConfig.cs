102e1fc4f26d17f31fec3d1afa59100c39f2af79
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-01-17 13:16:02 +0200

    feat: dd Swagger configuration with JWT authentication support       - Configures Swagger to include Bearer token authentication       - Defines security scheme and security requirements for API documentation

---
+++ sayHello.Backend/sayHello.api/SwaggerConfig.cs
@@ -0,0 +1,41 @@
+using Microsoft.OpenApi.Models;
+
+namespace sayHello.api;
+
+public static class SwaggerConfig
+{
+    public static void AddSwaggerConfig(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Say Hello | Real Time Chat App", Version = "v1" });
+
+            // Add JWT Authentication
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description =
+                    "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+    }
+}
